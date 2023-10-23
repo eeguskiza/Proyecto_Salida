8408f87ddb9f485ae90f6a4fe8e52b6c837e0651
eeguskiza <e.eguskiza@opendeusto.es>
2023-10-23 19:54:09 +0200

    commit
    splash screen actualizada

--- src/app/SplashScreen.java
+++ src/app/SplashScreen.java
@@ -1,6 +1,52 @@
 package app;
 
 import javax.swing.*;
-public class SplashScreen extends JFrame {
-    
-}
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SplashScreen extends JWindow {
+    private int duration;
+    private JProgressBar progressBar;
+
+    public SplashScreen(int d) {
+        duration = d;
+    }
+
+    public void showSplash() {
+        JPanel content = (JPanel) getContentPane();
+        content.setBackground(Color.white);
+
+        int width = 400;
+        int height = 100;
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screen.width - width) / 2;
+        int y = (screen.height - height) / 2;
+        setBounds(x, y, width, height);
+
+        progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+
+        content.add(progressBar, BorderLayout.CENTER);
+
+        setVisible(true);
+
+        try {
+            for (int i = 0; i <= 100; i++) {
+                final int progress = i;
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        progressBar.setValue(progress);
+                        progressBar.setString(progress + "%");
+                    }
+                });
+                Thread.sleep(duration / 100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setVisible(false);
+    }
+    }
--- src/app/main.java
+++ src/app/main.java
@@ -3,9 +3,10 @@ package app;
 public class main {
 
 	public static void main(String[] args) {
-		//SplashScreen primaryScreen = new SplashScreen();
-		//primaryScreen.setVisible(true);
-		
+		int duration = 2500; // Duración en milisegundos (5 segundos en este caso)
+		SplashScreen splash = new SplashScreen(duration);
+		splash.showSplash();
+
 		VentanaInicioSesion v1 = new VentanaInicioSesion();
 		v1.setVisible(true);
 		

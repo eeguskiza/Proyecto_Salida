91bb36a878e37a8023e873a324db44d30048f9e8
eeguskiza <e.eguskiza@opendeusto.es>
2023-11-15 16:38:41 +0100

    solo se crean clientes

--- src/GUI/Escritorio/Registro.java
+++ src/GUI/Escritorio/Registro.java
@@ -13,24 +13,25 @@ import java.text.SimpleDateFormat;
 public class Registro extends JFrame {
 
     public Registro(JFrame padre) {
+
+        Object[] opciones = {"Cliente", "Dueño"};
+        int seleccion = JOptionPane.showOptionDialog(null,
+                "Seleccione el tipo de usuario:",
+                "Tipo de Usuario",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null,
+                opciones, opciones[0]);
+
+        if (seleccion == JOptionPane.CLOSED_OPTION) {
+            // El usuario cerró el diálogo, podrías cerrar la aplicación o manejarlo de otra manera
+            return;
+        }
+
         this.setTitle("Registro");
         this.setSize(500, 500);
         this.setLocationRelativeTo(padre);
 
         JPanel panel = new JPanel(new GridLayout(0, 2, 10, 10)); // Layout para los campos y etiquetas
 
-        // Crear campos de texto y etiquetas para cada atributo del Usuario
-        panel.add(new JLabel("Tipo de Usuario:", JLabel.CENTER));
-        ButtonGroup tipoUsuario = new ButtonGroup();
-        JRadioButton cliente = new JRadioButton("Cliente");
-        JRadioButton dueño = new JRadioButton("Dueño");
-        tipoUsuario.add(cliente);
-        tipoUsuario.add(dueño);
-
-        JPanel panelTipoUsuario = new JPanel();
-        panelTipoUsuario.add(cliente);
-        panelTipoUsuario.add(dueño);
-        panel.add(panelTipoUsuario);
 
         panel.add(new JLabel("ID:", JLabel.CENTER));
         panel.add(new JTextField("@",20));
@@ -63,10 +64,21 @@ public class Registro extends JFrame {
         JButton cancelar = new JButton("Cancelar");
 
         aceptar.addActionListener(e -> {
-            String id = ((JTextField) panel.getComponent(3)).getText();
-            String nombre = ((JTextField) panel.getComponent(5)).getText();
-            String apellido = ((JTextField) panel.getComponent(7)).getText();
+            String id = ((JTextField) panel.getComponent(1)).getText();
+            String nombre = ((JTextField) panel.getComponent(3)).getText();
+            String apellido = ((JTextField) panel.getComponent(5)).getText();
             String fechaNacimiento = null;
+
+            Boolean tipoUsuarioSeleccionado = false; //false = cliente, true = dueño
+
+            if (seleccion == 0) {
+                tipoUsuarioSeleccionado = true;
+            } else if (seleccion == 1) {
+                tipoUsuarioSeleccionado = false;
+            } else {
+                JOptionPane.showMessageDialog(this, "Por favor, seleccione un tipo de usuario.", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
 
             // Fecha check
             if (dateChooser.getDate() != null) {
@@ -77,10 +89,10 @@ public class Registro extends JFrame {
                 return;
             }
 
-            String contraseña = new String(((JPasswordField) panel.getComponent(11)).getPassword());
-            String confirmarContraseña = new String(((JPasswordField) panel.getComponent(13)).getPassword());
-            String telefono = ((JTextField) panel.getComponent(15)).getText();
-            String correo = ((JTextField) panel.getComponent(17)).getText();
+            String contraseña = new String(((JPasswordField) panel.getComponent(9)).getPassword());
+            String confirmarContraseña = new String(((JPasswordField) panel.getComponent(11)).getPassword());
+            String telefono = ((JTextField) panel.getComponent(13)).getText();
+            String correo = ((JTextField) panel.getComponent(15)).getText();
 
 
             if (id.isEmpty() || nombre.isEmpty() || apellido.isEmpty() ||
@@ -90,23 +102,26 @@ public class Registro extends JFrame {
                 JOptionPane.showMessageDialog(this, "Por favor, revise todos los campos.", "Error", JOptionPane.ERROR_MESSAGE);
             } else {
                 try {
-                    if (dueño.isSelected()) {
+                    if (tipoUsuarioSeleccionado.equals(true)) {
                         Dueño nuevoUsuario = new Dueño(id, nombre, apellido, fechaNacimiento, contraseña, telefono, correo, new ArrayList<>());
                         System.out.println(nuevoUsuario.toString());
-                    } else if (cliente.isSelected()) {
+                    } else {
                         Cliente nuevoUsuario = new Cliente(id, nombre, apellido, fechaNacimiento, contraseña, telefono, correo, new ArrayList<>());
                         System.out.println(nuevoUsuario.toString());
                     }
 
                     JOptionPane.showMessageDialog(this, "Usuario creado con éxito!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
-                    MainMenu ventanaInicio = new MainMenu();
-                    this.dispose();
-                    padre.dispose();
-                    ventanaInicio.setVisible(true);
+
 
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(this, "Error al crear el usuario: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
                 }
+
+                MainMenu ventanaInicio = new MainMenu();
+                this.dispose();
+                padre.dispose();
+                ventanaInicio.setVisible(true);
+
             }
         });
 

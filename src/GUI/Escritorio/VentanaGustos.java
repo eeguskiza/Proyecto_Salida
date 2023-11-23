b219c0d5d0232afd951cb6baee740db5646f7b13
Alex Jauregui <alex.jauregui@opendeusto.es>
2023-11-23 09:40:34 +0100

    jesucristo porfavor

--- src/GUI/Escritorio/VentanaGustos.java
+++ src/GUI/Escritorio/VentanaGustos.java
@@ -1,21 +1,24 @@
-package GUI.Escritorio;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import GUI.Bienvenido;
+
 import com.toedter.calendar.JCalendar;
+
 
 public class VentanaGustos extends JFrame {
 
+
     public VentanaGustos() {
-        JPanel panelPrincipal = new JPanel();
+        JPanel panelPrincipal = new JPanel((new GridLayout(6, 1)));
         panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.Y_AXIS));
+
 
         JLabel preguntaLabel = new JLabel("¿Cuándo salimos?");
         panelPrincipal.add(preguntaLabel);
+
 
         String[] opciones = {"Ahora", "Luego"};
         JComboBox<String> comboBox = new JComboBox<>(opciones);
@@ -25,16 +28,27 @@ public class VentanaGustos extends JFrame {
         comboBox.setAlignmentX(Component.LEFT_ALIGNMENT);
         panelPrincipal.add(comboBox);
 
+
         JCalendar calendar = new JCalendar();
         calendar.setVisible(false);
         panelPrincipal.add(calendar);
+
 
         comboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 calendar.setVisible(comboBox.getSelectedItem().equals("Luego"));
+                System.out.println(calendar.getDate());
+                cambiarTamanoVentana(400,450);
+
+
+
+
             }
         });
+
+
+
 
         // Usando GridLayout para organizar los gustos en múltiples columnas
         JPanel gustosPanel = new JPanel(new GridLayout(0, 3, 1, 1)); // 0 filas significa cualquier cantidad de filas, 2 columnas, y espacio entre elementos
@@ -44,22 +58,49 @@ public class VentanaGustos extends JFrame {
                 "Karaoke", "Pista de baile", "Juegos de mesa", "Transmisión de deportes", "Wifi gratuito",
                 "Aire acondicionado", "Zona de fumadores", "Accesibilidad para discapacitados",
                 "Estacionamiento privado", "Pet friendly"};
+        ArrayList<String>gustosos=new ArrayList<String>();
 
-        for (String gusto : gustos) {
-            JCheckBox checkBoxGusto = new JCheckBox(gusto);
+
+        for (String gusto : gustos) {// mira gusto a gusto
+            JCheckBox checkBoxGusto = new JCheckBox(gusto);//crea un checkbox para cada gusto y lo añade al panel
             gustosPanel.add(checkBoxGusto);
+            checkBoxGusto.addActionListener(new ActionListener() {// le damos funcion a cada checkbox
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (checkBoxGusto.isSelected()){
+                        System.out.println("has seleccionado "+ checkBoxGusto.getText());
+                        gustosos.add(checkBoxGusto.getText());
+                    }else{
+                        gustosos.remove(checkBoxGusto.getText());
+                        System.out.println(checkBoxGusto.getText()+" deseleccionado");
+                        gustosos.add(checkBoxGusto.getText());
+                    }
+                }
+            });
         }
+
+
+
+
+
+
+
 
         // Añadir el panel de gustos al panel principal y alinear a la izquierda
         gustosPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
         panelPrincipal.add(gustosPanel);
 
+
         this.add(panelPrincipal);
-        this.setSize(500, 600);
+        this.setSize(400, 300); // Puede que necesites ajustar el tamaño para que todo quepa bien
         this.setVisible(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    private void cambiarTamanoVentana(int ancho, int alto) {
+        this.setSize(ancho, alto);
+    }
+
 
     public static void main(String[] args) {
         try {
@@ -69,9 +110,12 @@ public class VentanaGustos extends JFrame {
             e.printStackTrace();
         }
 
+
         SwingUtilities.invokeLater(() -> {
             new VentanaGustos();
         });
 
+
     }
 }
+

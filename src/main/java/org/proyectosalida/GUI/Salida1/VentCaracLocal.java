b59af3d9f7c2084b68360cbfe45f7919657b8459
Alex Jauregui <alex.jauregui@opendeusto.es>
2023-12-07 18:15:05 +0100

    VentSelectCarac añadido acepatr y conectar con tabla

--- src/main/java/org/proyectosalida/GUI/Salida1/VentCaracLocal.java
+++ src/main/java/org/proyectosalida/GUI/Salida1/VentCaracLocal.java
@@ -1,12 +1,42 @@
 package org.proyectosalida.GUI.Salida1;
 
+import com.teamdev.jxbrowser.browser.Browser;
+import com.teamdev.jxbrowser.engine.Engine;
+import com.teamdev.jxbrowser.engine.EngineOptions;
+import com.teamdev.jxbrowser.engine.RenderingMode;
+import com.teamdev.jxbrowser.view.swing.BrowserView;
 import org.proyectosalida.Constructores.Local;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VentCaracLocal extends JFrame {
     public VentCaracLocal(Local local) {
+        JPanel panelprincipal=new JPanel(new GridLayout(0,2));
+        JLabel nombre=new JLabel("Nombre");
+        panelprincipal.add(nombre);
+        JTextField Nombre =new JTextField(local.getNombre());
+        panelprincipal.add(Nombre);
+        JLabel direccion=new JLabel("Direccion");
+        panelprincipal.add(direccion);
+        JPanel Direccion =new JPanel();
+        BrowserView ubi = cargarMapa(local);
+        Direccion.add(ubi);
+        panelprincipal.add(Direccion);
 
+
+
+
+
+
+
+
+
+
+
+        this.add(panelprincipal);
+        this.setVisible(true);
+        this.setSize(300,500);
 
     }
 
@@ -18,5 +48,26 @@ public class VentCaracLocal extends JFrame {
                 new VentCaracLocal(null);
             }
         });
+    }
+    private BrowserView cargarMapa(Local local){
+        // Configuración de JxBrowser
+        String LICENSE_KEY = "6P830J66YCEA9SQDHRL8EDTZK57189867HR8YUD4L7QAI5ZHGIZK21JI39COT5XFHX0V";
+        EngineOptions options = EngineOptions.newBuilder(RenderingMode.HARDWARE_ACCELERATED)
+                .licenseKey(LICENSE_KEY)
+                .build();
+
+        // Inicializando el motor del navegador con la clave de licencia
+        Engine engine = Engine.newInstance(options);
+
+        // Creando el navegador
+        Browser browser = engine.newBrowser();
+
+        // Crear la vista del navegador para Swing
+        BrowserView view = BrowserView.newInstance(browser);
+
+        String url = "https://www.google.es/maps/"+local.getDireccion();
+        // Cargando la URL en el navegador
+        browser.navigation().loadUrl(url);
+        return view;
     }
 }
